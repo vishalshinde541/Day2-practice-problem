58f7e256cc5c682867ee49a3c8d7685d06ac3059
vishalshinde541 <vishalshinde541@gmail.com>
2022-09-24 07:51:35 +0530

    Read a single Digit Number and write in word

---
+++ src/com/bridgelabz/IfElseProblem/ReadDigitAndWriteInWords.java
@@ -0,0 +1,55 @@
+package com.bridgelabz.IfElseProblem;
+
+import java.util.Scanner;
+
+public class ReadDigitAndWriteInWords {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		
+		if (num == 0) {
+			System.out.println("Input number is zero");
+		}
+		else if (num == 1) {
+			System.out.println("Input number is One");
+			
+		}
+		else if (num == 2) {
+			System.out.println("Input number is Two");
+			
+		}
+		else if (num == 3) {
+			System.out.println("Input number is Three");
+			
+		}
+		else if (num == 4) {
+			System.out.println("Input number is Four");
+			
+		}
+		else if (num == 5) {
+			System.out.println("Input number is Five");
+			
+		}
+		else if (num == 6) {
+			System.out.println("Input number is Six");
+			
+		}
+		else if (num == 7) {
+			System.out.println("Input number is Seven");
+			
+		}
+		else if (num == 8) {
+			System.out.println("Input number is Eight");
+			
+		}
+		else if (num == 9) {
+			System.out.println("Input number is Nine");
+			
+		}
+		
+			
+	}
+
+}

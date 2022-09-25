645d7d22481f9a671c594c093d29ea3925ecd06a
vishalshinde541 <vishalshinde541@gmail.com>
2022-09-25 20:39:02 +0530

    Programs With While Loop

---
+++ src/com/bridgelabz/WhileLoopProblems/SumOfNaturalNumbers.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.WhileLoopProblems;
+
+import java.util.Scanner;
+
+// sum of natural numbers using while loop
+
+public class SumOfNaturalNumbers {
+	
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		
+		//take number as input from user
+		Scanner scn = new Scanner(System.in);
+		int num1 = scn.nextInt();
+		
+		//use while loop for calculating the sum of natural number
+		int i = 1;
+		while (i <= num1) {
+			
+			sum = sum + i;
+			
+			i++;
+		}
+		
+		//print the value of sum in console tab
+		System.out.println(sum);
+	}
+
+}
